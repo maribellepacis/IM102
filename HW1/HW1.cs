91582f7bfcba10ef39d49d16b95dcb4ade3af95f
maribellepacis <maribellepacis.9232@gmail.com>
2025-09-10 00:23:40 +0800

    HW1

---
+++ HW1/HW1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            do
+            {
+                Console.Write("Enter string: ");
+                input = Console.ReadLine();
+
+                if (input == "*")
+                    break;
+
+                string original = input;
+                string lower = input.ToLower();
+
+                // Manual reverse
+                string reversed = "";
+                for (int i = lower.Length - 1; i >= 0; i--)
+                {
+                    reversed += lower[i];
+                }
+
+                // Check if palindrome
+                string result = (lower == reversed) ? "Yes" : "No";
+
+                // Display result
+                Console.WriteLine("\nOriginal String         Reversed String            Palindrome?");
+                Console.WriteLine($"  {original,-20}{reversed,-25}{result}\n");
+
+            } while (true);
+
+            Console.WriteLine("\n//End execution");
+        }
+    }
+}
